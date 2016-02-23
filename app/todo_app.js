6224bd3a9f16d9fe6135a76983f319376b33d7f0
timcmiller <timcmiller@live.com>
2016-02-22 16:02:19 -0800

    initial commit

---
+++ app/todo_app.js
@@ -0,0 +1,23 @@
+import React,
+{
+  Component,
+  ListView,
+  Text,
+  View,
+  TouchableHighlight,
+} from 'react-native';
+
+export default class ToDoApp extends Component {
+  constructor(props) {
+    super(props);
+  }
+
+  render() {
+    return (
+      <View>
+        <Text>Jobs</Text>
+      </View>
+    );
+
+  }
+}
---
+++ main.js
@@ -0,0 +1,4 @@
+import { AppRegistry } from 'react-native';
+import ToDoApp from './app/todo_app';
+
+AppRegistry.registerComponent('main', () => ToDoApp);
